8103f318453e9d481d047dd1c9cc872475365ed3
Jason Beggs <jason@roasted.dev>
2020-05-26 15:28:33 -0400

    Initial setup

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'production' => false,
+    'baseUrl' => '',
+    'title' => 'Northeastern University - City Community Advancement',
+    'description' => 'We bring together community organizations, social service groups, government agencies, and Northeastern students, faculty, and staff to build a better world. A spirit of collaboration infuses everything we do, from hosting volunteer programs, to conducting service-based research, to organizing community events.',
+    'collections' => [],
+];
