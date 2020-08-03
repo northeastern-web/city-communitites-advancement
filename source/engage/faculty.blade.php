d3659666e17e962d89542919de042b1ba4585ff9
Jason Beggs <jason@roasted.dev>
2020-08-03 16:40:14 -0400

    Update faculty hero

--- source/engage/faculty.blade.php
+++ source/engage/faculty.blade.php
@@ -6,11 +6,10 @@ title: Students
 
 @section('hero')
     <div
-        class="py-16 bg-gray-800 bg-cover bg-no-repeat bg-top"
-        style="background-image: url('/assets/images/engage-students-hero.jpg')"
+        class="py-16 bg-black"
     >
         <div class="container">
-            <div class="max-w-xl w-full px-8 py-12 text-white bg-black-semi-9 lg:px-12 lg:py-16">
+            <div class="max-w-xl w-full text-white">
                 <h1 class="text-3xl leading-none md:text-5xl">Faculty</h1>
                 <p class="mt-6 text-gray-200">City and Community Engagement offers several opportunities and programs for Northeastern faculty to engage with Boston residents and community organizations, including nonprofits and schools. We’ll work with you to develop service-learning courses, conduct research with a nonprofit, or co-host an event with a community organization, to name a few examples.</p>
             </div>
