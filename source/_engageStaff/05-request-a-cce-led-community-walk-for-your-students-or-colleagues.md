8a06baac92790d1d36fa113c98f7ce61635b0bef
Jason Beggs <jason@roasted.dev>
2020-08-05 13:56:02 -0400

    Add request a walk page

--- source/_engageStaff/05-request-a-cce-led-community-walk-for-your-students-or-colleagues.md
+++ source/_engageStaff/05-request-a-cce-led-community-walk-for-your-students-or-colleagues.md
@@ -1,5 +1,5 @@
 ---
 title: Request a CCE-led community walk for your students or colleagues
 description: Provide your students or office team the opportunity to learn more about the neighborhoods near Northeastern’s Boston campus.
-link_to: '#'
+link_to: /engage/request-a-community-walk-led-by-our-staff
 ---
---
+++ source/engage/request-a-community-walk-led-by-our-staff.blade.php
@@ -0,0 +1,25 @@
+---
+title: Request a Community Walk Led By Our Staff
+description: Community walks led by the Office of City and Community Engagement introduce Northeastern students, faculty, and staff to the neighborhoods that Northeastern is a part of by highlighting existing community partnerships and exploring citywide initiatives.
+
+---
+
+@extends('_layouts.engage-child')
+
+@section('content')
+<div class="prose max-w-3xl lg:prose-lg">
+    <h1>{!! $page->title !!}</h1>
+    <p class="lead">{{ $page->description }}</p>
+    <p>A community walk is a 60-75-minute walk for a small group (10–15) of Northeastern students, faculty, and/or staff through one or more of the following Boston neighborhoods: Roxbury, Fenway, Mission Hill, and the South End. Along the walk, a staff or student facilitator provides information about community engagement, the history of the area, community partnerships, and citywide initiatives.</p>
+    <p>The information—culled from extensive research and community input—is meant to help participants explore and understand how our changing city and systemic issues affect our campus and local neighborhoods, and vice versa.</p>
+    <p>City and Community Engagement trains students to lead the community walks We also collaborate with campus partners to train others to lead the tours for special initiatives. We’re happy to share a copy of our materials for you to explore the community walks on your own.</p>
+    <p>Examples of some recent community walks organized in collaboration with campus partners:</p>
+    <ul>
+        <li>Northeastern professors requested a CCE student leader to lead a community walk during the first week of class for students in their service-learning courses.</li>
+        <li>CCE partnered with the University Honors Program to host community walks for their first-year seminar course of 300+ students.</li>
+        <li>First-year students in the Northeastern Alliance of Civically Engaged Students program went on CCE-led community walks in order to explore and better understand Boston neighborhoods as new members of the Northeastern community.</li>
+    </ul>
+    <h2>Contact</h2>
+    <p>Please email our team at <a href="mailto:communityservice@northeastern.edu">communityservice@northeastern.edu</a> if you’re interested in learning more about or scheduling a community walk for your group. A member of our team will be in touch within 3–5 business days to discuss how we can best fulfill your request. We typically need a minimum of two weeks notice to try to secure facilitators for CCE-led walks and a minimum of four weeks for campus partner collaborations.</p>
+</div>
+@endsection
