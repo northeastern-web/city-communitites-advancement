74f5f357ef8a31fa40bd770975f6a1e9cd327cb1
Jason Beggs <jason@roasted.dev>
2020-08-05 13:32:34 -0400

    Add Co-host a program page

---
+++ source/engage/co-host-a-program.blade.php
@@ -0,0 +1,24 @@
+---
+title: Co-host a Program with a Community Organization or City & Community Engagement
+description: Build new connections with off-campus communities by organizing a public event, such as a lecture, panel, workshop, or forum.
+---
+
+@extends('_layouts.engage-child')
+
+@section('content')
+<div class="prose max-w-3xl lg:prose-lg">
+    <h1>{!! $page->title !!}</h1>
+    <p class="lead">{{ $page->description }}</p>
+    <h2>Plan an Event</h2>
+    <p>City and Community Engagement helps connect members of the Northeastern community to residents and organizations in neighborhoods adjacent to the university’s Boston campus. These connections foster new relationships, enhance learning outcomes, and increase the understanding of issues and challenges on- and off-campus. </p>
+    <p>We work with other Northeastern departments to host interactive engagements designed to support area residents, such as a legal clinic run by law students for individuals in need of advice. We also introduce Northeastern faculty to knowledgeable local leaders, which can then lead to collaborative workshops and open dialogues with students. For example, an advocacy class and a local nonprofit jointly organized an interactive forum on how students can be good neighbors in local communities.</p>
+    <p>The most successful collaborations are short-term, highly focused, and mutually beneficial. Our staff assists faculty with identifying local partners and speakers. We can also advise on socially- and culturally-relevant approaches to sensitive community issues as needed.</p>
+    <h2>Contact</h2>
+    <p>
+        Marisa Luse</br>
+        Campus Engagement and Collaborations Manager</br>
+        <a href="mailto:m.luse@northeastern.edu">m.luse@northeastern.edu</a></br>
+        617.373.8012
+    </p>
+</div>
+@endsection
