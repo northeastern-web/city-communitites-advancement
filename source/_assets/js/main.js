11f492e9ad8290afc0b3af9d63c878912c5e8ee5
Jason Beggs <jason@roasted.dev>
2020-08-06 08:55:12 -0400

    Remove more code

--- source/_assets/js/main.js
+++ source/_assets/js/main.js
@@ -1,52 +1,6 @@
 require('@northeastern-web/kernl-ui');
 
 import lunr from 'lunr';
-import { format, add } from 'date-fns';
-
-window.contentful = require('contentful');
-
-window.mergeObjects = function(a, b) {
-    return { ...a, ...b };
-};
-
-window.contentfulServiceOpportunities = function(space, accessToken) {
-    return {
-        loading: false,
-        client: null,
-        entries: [],
-        search: '',
-        date: '',
-        init() {
-            this.client = contentful.createClient({
-                space,
-                accessToken,
-            });
-
-            this.getEntries();
-
-            this.$watch('search', () => this.getEntries());
-            this.$watch('date', () => this.getEntries());
-        },
-        getEntries() {
-            this.loading = true;
-
-            this.client
-                .getEntries({
-                    content_type: 'serviceOpportunity',
-                    query: this.search,
-                    order: 'sys.createdAt',
-                })
-                .then(data => {
-                    this.entries = data.items;
-
-                    this.loading = false;
-                })
-                .catch(err => {
-                    this.loading = false;
-                });
-        },
-    };
-};
 
 window.searchIndex = lunr(function() {
     this.ref('slug');
