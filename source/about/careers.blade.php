c2aea13f14f59e1c7ff6cecaa7e1e65dce448980
Jason Beggs <jason@roasted.dev>
2020-08-20 10:21:47 -0400

    Update link and copy

--- source/about/careers.blade.php
+++ source/about/careers.blade.php
@@ -16,13 +16,13 @@ description: Our Office of City and Community Engagement is always looking for g
         <p>Northeastern students interested in co-op positions with our team should visit the university’s <a href="https://northeastern.edu/hrm/careers/">Careers page</a> and search for the Office of City and Community Engagement.</p>
 
         <h3>Graduate Assistants</h3>
-        <p>We post CCE graduate assistant positions on Northeastern’s <a href="https://studentemployment.neu.edu/">Student Employment page</a>.</p>
+        <p>We post CCE graduate assistant positions on Northeastern’s <a href="https://northeastern-csm.symplicity.com/students/?signin_tab=0">NUworks</a>.</p>
 
         <h3>Jumpstart</h3>
         <p>Northeastern students interested in working off-campus as teachers aides in preschool classes are encouraged to apply for the <a href="/programs/jumpstart">Jumpstart program</a>.</p>
 
         <h3>Work-Study</h3>
-        <p>We post CCE work-study positions on Northeastern’s Student Employment page. 
+        <p>We post CCE work-study positions on Northeastern’s Student Employment page.
         <h2>Non-Student Positions</h2>
         <h3>AmeriCorps (Massachusetts Promise Fellowship)</h3>
         Each year, Northeastern hires 40 full-time AmeriCorps Massachusetts Promise Fellows—who are often recent college graduates—to lead out-of-school time programs for youth in grades 6–12 in low-income communities. <a href="/programs/massachusetts-promise-fellowship">Learn more and apply.</a></p>
