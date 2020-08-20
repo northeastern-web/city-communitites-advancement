cedc36c7a33bbbb212da3e950ec7599a8d9b240a
Jason Beggs <jason@roasted.dev>
2020-08-20 10:23:55 -0400

    Update copy

--- source/engage/request-a-cce-presentation.blade.php
+++ source/engage/request-a-cce-presentation.blade.php
@@ -13,6 +13,6 @@ description: Are you interested in learning more about the Office of City and Co
 <div class="prose max-w-3xl lg:prose-lg">
     <p>{{ $page->description }} Do you want to help students or colleagues serve, volunteer, or collaborate with communities in and near Northeastern’s Boston campus?  A member of our staff can give a presentation to your students or office team about opportunities to engage with the community through CCE programs and events.</p>
 
-    <p>Form coming soon.</p>
+    <p>Email <a href="mailto:communityengagement@northeastern.edu">communityengagement@northeastern.edu</a> to connect with a team member.</p>
 </div>
 @endsection
