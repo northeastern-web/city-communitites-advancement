8b7771883d8a0e76086f6b000050fb3d5ee50948
Ray Kingston <r.kingston@neu.edu>
2020-09-14 14:52:17 -0400

    Update community partners content

--- source/about/partners.blade.php
+++ source/about/partners.blade.php
@@ -6,7 +6,7 @@ description: City and Community Engagement partners with a wide range of local o
 @extends('_layouts.about')
 
 @section('content')
-    <p class="mb-8">Below is the list of organizations that partner or have partnered with CCE’s signature programs.</p>
+    <p class="mb-8">Below is the list of organizations that partner or have partnered with CCE’s signature programs. In addition to the organizations listed here, CCE maintains a variety of partnerships that support ongoing events and initiatives across our network.</p>
     <x-kernl-accordion.base label="Program partners accordion">
         <x-kernl-accordion.item>
             <x-slot name="title">
@@ -19,16 +19,14 @@ description: City and Community Engagement partners with a wide range of local o
             <div class="text-gray-700">
                 <ul>
                     <li>7 Elements</li>
-                    <li>A Fare Start/Northwest Harvest</li>
                     <li>American Hiking Society</li>
                     <li>Best Friends Animal Society</li>
-                    <li>Cleveland LBGT Community Center</li>
+                    <li>Hope House</li>
                     <li>Project Cure</li>
                     <li>Saguaro National Park</li>
                     <li>Outreach 360</li>
                     <li>Peaceable Kingdom Retreat for Children</li>
                     <li>Plenitud</li>
-                    <li>Tandana Foundation</li>
                 </ul>
             </div>
         </x-kernl-accordion.item>
@@ -43,16 +41,12 @@ description: City and Community Engagement partners with a wide range of local o
 
             <div class="text-gray-700">
                 <ul>
-                    <li>Apprentice Learning</li>
+                    <li>826 Boston</li>
                     <li>Community Servings</li>
-                    <li>Cradles to Crayons</li>
-                    <li>Hosteling International</li>
-                    <li>Jewish Vocational Service</li>
+                    <li>Hernández After School Program</li>
+                    <li>Hyde Square Task Force</li>
                     <li>Let’s Get Ready</li>
-                    <li>Little Brothers Friends of the Elderly</li>
                     <li>Orchard Gardens Boys and Girls Club</li>
-                    <li>SquashBusters</li>
-                    <li>St. Francis House</li>
                 </ul>
             </div>
         </x-kernl-accordion.item>
@@ -84,41 +78,33 @@ description: City and Community Engagement partners with a wide range of local o
 
             <div class="text-gray-700">
                 <ul>
-                    <li>“e” inc.</li>
-                    <li>Artists for Humanity</li>
-                    <li>Big Brothers Big Sisters of Central Mass/Metrowest</li>
-                    <li>Boston Public Health Commission</li>
-                    <li>Boston Public Schools Office of Human Capital</li>
-                    <li>Breakthrough Greater Boston</li>
-                    <li>BUILD</li>
-                    <li>Cambridge Community Services</li>
-                    <li>Cape Verdean Association</li>
-                    <li>Charlestown High School (Boston Public Schools)</li>
-                    <li>Dana-Farber Cancer Institute</li>
-                    <li>East End House</li>
-                    <li>Flying Cloud Institute</li>
-                    <li>Freedom House, Inc.</li>
-                    <li>Girls Inc. of Lynn</li>
-                    <li>Greater Fall River Re-Creation</li>
+                    <li>Artists For Humanity</li>
+                    <li>Asian Community Development Corporation</li>
+                    <li>Barnstable Public Schools</li>
+                    <li>Boston Chinatown Neighborhood Center</li>
+                    <li>Boys & Girls Clubs of MetroWest</li>
+                    <li>Brockton Public Schools</li>
+                    <li>Children's Services of Roxbury</li>
+                    <li>Enroot Inc.</li>
+                    <li>Framingham Public Schools</li>
+                    <li>Freedom House</li>
+                    <li>Friends of the Hernández</li>
+                    <li>Girls Incorporated of Lynn</li>
+                    <li>Girls' LEAP</li>
                     <li>Health Resources in Action</li>
-                    <li>Hernandez After School Program</li>
-                    <li>Joseph M. Tierney Learning Center</li>
-                    <li>LEAP for Education</li>
-                    <li>Mass General Hospital Center for Community Health Improvement Youth Programs</li>
-                    <li>Massachusetts Coalition for Occupational Safety and Health</li>
-                    <li>MathPOWER</li>
+                    <li>Improbable Players</li>
+                    <li>La Vida Scholars </li>
+                    <li>Margarita Muñiz Academy</li>
+                    <li>Mass General Brigham - Center for Community Health Improvement </li>
+                    <li>MetroWest College Planning Collaborative</li>
+                    <li>MissionSAFE</li>
                     <li>Northeastern University Marine Science Center</li>
-                    <li>Old Colony YMCA Big Sister Big Brother</li>
-                    <li>Quincy Asian Resources Inc.Mentoring</li>
-                    <li>Regional Environmental Council</li>
-                    <li>St. Stephen’s Youth Programs</li>
-                    <li>Stoughton Youth Commission</li>
-                    <li>Tutoring Plus of Cambridge, Inc.</li>
-                    <li>uAspire</li>
-                    <li>Waltham High School</li>
-                    <li>West End House, Inc.</li>
+                    <li>Preservation of Affordable Housing, Inc.</li>
+                    <li>SquashBusters</li>
+                    <li>SquashBusters Lawrence</li>
+                    <li>St.Stephen's Youth Program</li>
+                    <li>Town of Stoughton</li>
                     <li>Youth Enrichment Services</li>
-                    <li>ZUMIX</li>
                 </ul>
             </div>
         </x-kernl-accordion.item>
@@ -165,161 +151,47 @@ description: City and Community Engagement partners with a wide range of local o
             <x-slot name="title">
                 <span class="text-gray-600 text-xs uppercase tracking-widest">Program</span>
                 <div>
-                    <strong>Northeastern-Based (On-Campus) Partners</strong>
-                </div>
-            </x-slot>
-
-            <div class="text-gray-700">
-                <ul>
-                    <li>Bridge to Calculus</li>
-                    <li>Family Gym</li>
-                    <li>Human Rights Organization</li>
-                    <li>Husky Environmental Action Team</li>
-                    <li>Marine Science Center</li>
-                    <li>Northeastern ASL</li>
-                    <li>Northeastern Students4Giving</li>
-                    <li>Peace Through Play</li>
-                    <li>Slow Food</li>
-                </ul>
-            </div>
-        </x-kernl-accordion.item>
-
-        <x-kernl-accordion.item>
-            <x-slot name="title">
-                <span class="text-gray-600 text-xs uppercase tracking-widest">Program</span>
-                <div>
                     <strong>Service-Learning</strong>
                 </div>
             </x-slot>
 
             <div class="text-gray-700">
                 <ul>
-                    <li>826 Boston</li>
-                    <li>Access Sport America</li>
                     <li>Action for Boston Community Development (ABCD)</li>
-                    <li>Action for Boston Community Development (ABCD) – University High School</li>
-                    <li>Active Minds</li>
-                    <li>Art Resource Collaborative</li>
-                    <li>Asian Community Development Corporation</li>
-                    <li>Balfour Academy</li>
-                    <li>BalletRox</li>
-                    <li>Beachmont School STEAM Club</li>
-                    <li>Beacon Hill Village</li>
-                    <li>Belmont SPORT</li>
-                    <li>Bikes Not Bombs</li>
-                    <li>Bird Street Community Center</li>
-                    <li>Boston Breakers</li>
-                    <li>Boston Building Resources</li>
-                    <li>Boston Cares</li>
-                    <li>Boston Chinatown Neighborhood Center</li>
-                    <li>Boston Debate League</li>
+                    <li>Afrimerican Culture Initiative Inc.</li>
+                    <li>Beacon Communities</li>
+                    <li>Black Cotton Club</li>
                     <li>Boston Housing Authority</li>
-                    <li>Boston Public Schools – Boston Latin Academy</li>
-                    <li>Boston Public Schools – Central Administration</li>
-                    <li>Boston Public Schools – Edward M. Kennedy Academy for Health Careers</li>
-                    <li>Boston Public Schools – William Carter School</li>
-                    <li>Boston Scores</li>
-                    <li>Bowdoin Apartments</li>
-                    <li>Boys & Girls Clubs of Boston – Hennigan Club</li>
-                    <li>Boys & Girls Clubs of Boston – Jordan Club</li>
-                    <li>Boys & Girls Clubs of Boston – Sumner Club</li>
-                    <li>Boys & Girls Clubs of Boston – Yawkey Club of Roxbury</li>
-                    <li>Breakthrough Greater Boston</li>
-                    <li>Camp Harbor View</li>
-                    <li>Castle Square Tenants Organization</li>
-                    <li>Catholic Charities of Boston – El Centro del Cardenal</li>
-                    <li>Catholic Charities of Boston – Laboure Center</li>
-                    <li>Catholic Charities of Boston – Teen Center at St. Peter’s</li>
-                    <li>CharlesNewtown Housing Cooperative</li>
-                    <li>Chester Square Neighborhood Association</li>
+                    <li>Bryce's Journey, Inc.</li>
+                    <li>Cardinal Works</li>
+                    <li>Catholic Charities of Boston, El Centro del Cardenal</li>
                     <li>Citizen Schools</li>
-                    <li>City of Arlington</li>
-                    <li>City of Cambridge</li>
-                    <li>City of Chelsea</li>
-                    <li>City of Newton</li>
-                    <li>City of Revere</li>
-                    <li>City of Somerville</li>
-                    <li>Claremont Neighborhood Association</li>
-                    <li>Community Call</li>
-                    <li>Community Servings</li>
-                    <li>Daily Table</li>
-                    <li>Dig Boston</li>
-                    <li>Digital Grandparents</li>
-                    <li>EHS Alumni & Friends Tutoring Center</li>
-                    <li>Elderly Housing Development & Operations Corporation</li>
-                    <li>Ellis Memorial</li>
-                    <li>Emmanuel Gospel Center</li>
-                    <li>Engage Boston</li>
-                    <li>Everyday Boston</li>
+                    <li>Ecumenical Social Action Committee</li>
                     <li>EVkids</li>
-                    <li>Feminist Student Organization</li>
-                    <li>Fenway Community Center</li>
-                    <li>Fenway High School</li>
-                    <li>Fenway News</li>
-                    <li>Fit Kids at Madison Park</li>
-                    <li>For His Children (Ecuador)</li>
-                    <li>Franciscan Hospital Adaptive Ice Skating</li>
-                    <li>Franklin Park Coalition</li>
-                    <li>Franklin Park Zoo</li>
-                    <li>Generation Citizen</li>
-                    <li>Global Land Tenure Security Index</li>
-                    <li>Greater Boston Chinese Golden Age Center, Hong Lok House</li>
-                    <li>Greater Boston Chinese Golden Age Center, Quincy Towers</li>
-                    <li>Hattie B Cooper Community Center</li>
-                    <li>HEARTH at Elsie Frank House</li>
-                    <li>HEARTH at Ruggles Assisted Living</li>
-                    <li>Hernandez After School Program</li>
-                    <li>Horizons for Homeless Children</li>
-                    <li>Hosteling International Boston</li>
+                    <li>Family Gym Program</li>
+                    <li>Fenway Community Development Corporation</li>
+                    <li>Friends of the Hernandez</li>
+                    <li>Girls' LEAP</li>
+                    <li>H.E.R.E. Justice Coalition Hyde Park</li>
                     <li>Hyde Square Task Force</li>
-                    <li>Latino STEM Alliance</li>
-                    <li>Latinx Student Cultural Center</li>
-                    <li>Let’s Get Ready</li>
-                    <li>Literacy Volunteers of MA</li>
-                    <li>Little Brothers – Friends of the Elderly</li>
-                    <li>Lower Roxbury Coalition</li>
-                    <li>Madison Park Village</li>
-                    <li>Mason Place Housing Development</li>
-                    <li>Mass Children’s Alliance</li>
-                    <li>Mass Network for Foster Care Alumni</li>
-                    <li>Massachusetts Network of Foster Care Alumni</li>
-                    <li>Maverick Landing Community Services</li>
-                    <li>Morville House</li>
-                    <li>Mujeres Unidas Avanzando</li>
-                    <li>National Council for Adoption</li>
-                    <li>New Hope, Inc</li>
-                    <li>Old Colony YMCA</li>
+                    <li>Let's Get Ready</li>
+                    <li>Little Brothers - Friends of the Elderly</li>
+                    <li>Margarita Muniz Academy</li>
+                    <li>Mass Ave Coalition</li>
+                    <li>Mida</li>
+                    <li>Mujeres Unidas Avanzando </li>
+                    <li>Northeastern Center on Crime and Community Resilience</li>
+                    <li>Old North Church & Historic Site</li>
                     <li>Old West Church</li>
-                    <li>Raising a Reader</li>
-                    <li>Rebuilding Together Boston</li>
-                    <li>Resilient Sisterhood Project</li>
-                    <li>Round Table</li>
-                    <li>Scholar Athletes</li>
-                    <li>Shooting Touch</li>
-                    <li>Smarter in the City</li>
-                    <li>Social Impact Center</li>
-                    <li>Spaulding Adaptive Sports</li>
-                    <li>Speak for the Trees</li>
-                    <li>SquashBusters</li>
-                    <li>St. Mark Community Education</li>
-                    <li>St. Stephen’s Youth Programs</li>
-                    <li>Stearns Housing Development</li>
-                    <li>Sustainable Business Network</li>
-                    <li>Symphony Plaza West</li>
-                    <li>T.O.P.S Basketball</li>
-                    <li>T.O.P.S Soccer</li>
-                    <li>The Clubhouse Network</li>
-                    <li>The Sonny Walker Computer Center</li>
-                    <li>Timothy Smith Network</li>
+                    <li>Peace through Play</li>
+                    <li>Round Table, Inc</li>
+                    <li>South Boston Senior Housing & Center</li>
+                    <li>St. Mark Community Education Program</li>
                     <li>United South End Settlements</li>
-                    <li>Victory Programs</li>
-                    <li>Wang YMCA of Chinatown</li>
-                    <li>YES (Youth Enrichment Services)</li>
-                    <li>YMCA International Learning Center</li>
-                    <li>You’re With Us</li>
-                    <li>ZUMIX</li>
+                    <li>You're with Us</li>
                 </ul>
             </div>
         </x-kernl-accordion.item>
     </x-kernl-accordion.base>
+    <p class="mt-8">View a list of Fall 2013-Spring 2020 service-learning course community partners <a href="https://docs.google.com/document/d/14LMlIsFaQvmFGhGs-XNQKw9cJTM2ugXiqwpu_bUB92M/">here</a>.</p>
 @endsection
